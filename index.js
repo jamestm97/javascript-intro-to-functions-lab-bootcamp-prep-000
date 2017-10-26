690eeb4d4df2db87b15a9919dda33ce49b37e951
James McKay <jamestm97@gmail.com>
2017-10-26 17:48:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,19 @@ function shout(string) {
    if (string === string.toLowerCase()) {
      return("I can't hear you!");
    }
+<<<<<<< HEAD
 
    else if (string === string.toUpperCase()) {
      return("YES INDEED!");
    }
 
+=======
+  
+   else if (string === string.toUpperCase()) {
+     return("YES INDEED!");
+   }
+  
+>>>>>>> a787a279129e3548e7c5517d9730155adc08f9ba
    else if (string === string) {
      return("I love you, too.");
    }
